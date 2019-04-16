a38d069250fb179c85ac050a42de05476d57f335
Đỗ Tấn Khoa <khoadt2@vng.com.vn>
2019-04-16 21:44:45 +0700

    change template

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', layout: 'index/indexLayout' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Trang chủ',
+    topBrand: [{
+      image: '/images/apple.png',
+      name: 'Apple',
+      delay: 0,
+    }, {
+      image: '/images/samsung.png',
+      name: 'Samsung',
+      delay: 150,
+    }, {
+      image: '/images/xiaomi.jpg',
+      name: 'Xiaomi',
+      delay: 300,
+    }]
+  });
 });
 
 module.exports = router;
