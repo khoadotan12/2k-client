100445d9504a2227eecd8587b1bc23f1a2a1139b
Khoa Do <khoa400@gmail.com>
2019-04-21 16:02:52 +0700

    put data in shop page

--- routes/product.js
+++ routes/product.js
@@ -1,18 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req, res, next) => {
+    const data = {
+        brands: [{
+            name: 'samsung',
+            count: '162',
+        }, {
+            name: 'apple',
+            count: '33',
+        }, {
+            name: 'xiaomi',
+            count: '190'
+        }],
+        items: [{
+            image: '/images/apple.png',
+            name: 'Apple iPhone X 64 GB',
+            info: 'Hàng chính hãng',
+            price: '20,790,000',
+            uri: 'apple/2',
+        }, {
+            image: '/images/iphoneXSMax.png',
+            name: 'Apple iPhone XS Max 64 GB',
+            info: 'Hàng chính hãng',
+            price: '28,790,000',
+            uri: 'apple/1',
+        }, {
+            image: '/images/iphoneXSMax.png',
+            name: 'Apple iPhone XS Max 64 GB',
+            info: 'Hàng nhập khẩu',
+            price: '25,290,000',
+            uri: 'apple/3',
+        }, {
+            image: '/images/samsung.png',
+            name: 'Samsung Galaxy S10+',
+            info: 'Hàng chính hãng',
+            price: '21,390,000',
+            uri: 'samsung/4',
+        }]
+    };
+    res.render('product/all', { title: 'Cửa hàng', data });
+});
+
 router.get('/:category/:id', (req, res, next) => {
     const data = {
         name: 'iPhone XS Max 64 GB',
         brand: 'Apple',
         price: '28,790,000',
         color: ['Bạc', 'Vàng', 'Xám'],
-        shortInfo: ['Hệ điều hành: iOS 12', 'RAM: 4 GB', 'ROM: 64 GB', 'Chip xử lý: A12 Bionic 64-bit 7nm'],
+        shortInfo: ['Hệ điều hành: iOS 12', 'RAM: 4 GB', 'ROM: 64 GB', 'Chip xử lý: A12 Bionic 64-bit 7nm'],
         info: {
             screen: '6.5 inches',
             ram: '4 GB',
             rom: '64 GB',
-            frontCamera: '7MP, f / 2.2, 32mm',
+            frontCamera: '7 MP, f / 2.2, 32mm',
             backCamera: '12 MP',
             os: 'iOS 12',
             sim: '1',
