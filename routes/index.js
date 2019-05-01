e2a2a27e6026c115a7d0add52a812b668db1ed6e
Đỗ Tấn Khoa <khoadt2@vng.com.vn>
2019-05-01 16:54:55 +0700

    create home controller

--- routes/index.js
+++ routes/index.js
@@ -1,62 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const homeControllers = require('../controllers/homeControllers');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Trang chủ',
-    topBrand: [{
-      image: '/images/apple.png',
-      name: 'Apple',
-      delay: 0,
-    }, {
-      image: '/images/samsung.png',
-      name: 'Samsung',
-      delay: 150,
-    }, {
-      image: '/images/xiaomi.jpg',
-      name: 'Xiaomi',
-      delay: 300,
-    }],
-    hotItems: [{
-      name: 'iPhone XS Max 64 GB',
-      image: '/images/iphoneXSMax.png',
-      price: '23.490.000',
-      uri: '/apple/1',
-    }, {
-      name: 'Samsung Galaxy S9',
-      image: '/images/galaxyS9.png',
-      price: '17.890.000',
-      uri: '/samsung/2',
-    }, {
-      name: 'iPhone X 64 GB',
-      image: '/images/apple.png',
-      uri: '/apple/2',
-      price: '21.000.000'
-    }, {
-      name: 'Oppo F11 Pro',
-      image: '/images/oppof11pro.png',
-      uri: '/oppo/3',
-      price: '8.490.000'
-    }, {
-      name: 'Xiaomi Redmi Note 7',
-      image: '/images/xiaomi.jpg',
-      uri: '/xiaomi/1',
-      price: '4.090.000'
-    }],
-  });
-});
+router.get('/', homeControllers.home);
 
-router.get('/login', (req, res) => {
-  res.render('authen/login', {title: 'Đăng nhập'})
-});
+router.get('/login', homeControllers.loginGet);
 
-router.get('/recover', (req, res) => {
-  res.render('authen/recover', {title: 'Quên mật khẩu'})
-});
+router.get('/recover', homeControllers.recoverGet);
 
-router.get('/register', (req, res) => {
-  res.render('authen/register', {title: 'Đăng ký'})
-});
+router.get('/register', homeControllers.registerGet);
 
 module.exports = router;
