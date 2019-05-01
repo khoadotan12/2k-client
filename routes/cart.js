fa31c6ea96cc496d769d2f62c56902ede9bfb1ed
Đỗ Tấn Khoa <khoadt2@vng.com.vn>
2019-05-01 16:59:51 +0700

    create cart controllers

--- routes/cart.js
+++ routes/cart.js
@@ -1,27 +1,9 @@
-var express = require('express');
-var router = express.Router();
-function formatPrice(price) {
-    return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-router.get('/', (req, res, next) => {
-    const price = [4990000, 26590000];
-    const data = [{
-        name: 'Xiaomi Redmi Note 7',
-        price: formatPrice(price[0]),
-        image: '/images/xiaomi.jpg',
-        count: 1,
-    }, {
-        name: 'iPhone XS Max 64 GB',
-        price: formatPrice(price[1]),
-        image: 'images/iphoneXSMax.png',
-        count: 2,
-    }];
-    let sum = 0;
-    data.forEach((element, index) => {
-        element.total = formatPrice(price[index] * element.count);
-        sum += (price[index] * element.count);
-    });
-    res.render('cart/index', { title: 'Giỏ hàng', data, sum: formatPrice(sum) })
-});
+const express = require('express');
+const router = express.Router();
+
+const cartControllers = require('../controllers/cartControllers');
+
+
+router.get('/', cartControllers.index);
 
 module.exports = router
